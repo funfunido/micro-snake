24474320e9bcda95150e6ce3154c3bcdda12687c
funfunido <161741261+funfunido@users.noreply.github.com>
2025-03-06 10:35:48 -0700

    bro typescript is GOOFY AHH

--- main.ts
+++ main.ts
@@ -1,20 +1,30 @@
-let playerPosition = [0, 2];
+//basic.showString("Micro:Snake")
 
-let randomfood = [randint(0, 4,), randint(0, 4)];
+let playerPosition/*: [number, number]*/ = [0, 2];
+let randomfood/*: [number, number]*/ = [randint(0, 4), randint(0, 4)];
 
 
-if (randomfood === [playerPosition[0], playerPosition[1]]) {
-    let randomfood = [randint(0, 4,), randint(0, 4)];
 
+if (randomfood[0] === playerPosition[0] && randomfood[1] === playerPosition[1]) {
+    randomfood = [randint(0, 4), randint(0, 4)];
 }
 
 
-function startGame() {
+
+function refreshScreen() {
     led.toggle(playerPosition[0], playerPosition[1]);
     led.toggle(randomfood[0], randomfood[1]);
-
-
 }
 
+refreshScreen();
 
-startGame();
+
+
+input.onButtonPressed(Button.B, function () {
+    playerPosition[0] = (playerPosition[0] + 1) /*% 5*/;
+    refreshScreen();
+
+});
+
+//let exampleTuple: [number, number] = [5, 6]
+//let value: number = (exampleTuple as any[])[0]
