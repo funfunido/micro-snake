fc2063e234e17702f30e134ef97b75c7a8d4f89a
funfunido <161741261+funfunido@users.noreply.github.com>
2025-03-06 09:54:14 -0700

    better like this

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
-let randomfood = [randint(0, 4,), randint(0, 4)];
-    
-    
-    if (randomfood === [0,2]) {
-        let randomfood = [randint(0, 4,), randint(0, 4)];
+let playerPosition = [0, 2];
 
-    }
+let randomfood = [randint(0, 4,), randint(0, 4)];
+
+
+if (randomfood === [playerPosition[0], playerPosition[1]]) {
+    let randomfood = [randint(0, 4,), randint(0, 4)];
+
+}
 
 
 function startGame() {
-led.toggle(0,2);
-led.toggle(randomfood[0], randomfood[1]);
+    led.toggle(playerPosition[0], playerPosition[1]);
+    led.toggle(randomfood[0], randomfood[1]);
 
 
 }
