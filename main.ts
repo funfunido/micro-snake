592ab5f5785a6285ce5f8528d971f534be8779f5
funfunido <161741261+funfunido@users.noreply.github.com>
2025-03-14 09:50:46 -0600

    added code comments

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 basic.showString("Micro:Snake")
 
+
+//Sets player position and a random food position
 let playerPosition/*: [number, number]*/ = [0, 2];
 let randomfood/*: [number, number]*/ = [randint(0, 4), randint(0, 4)];
 
 
-
+// Makes sure the player position and random food can't be the same
 if (randomfood[0] === playerPosition[0] && randomfood[1] === playerPosition[1]) {
     randomfood = [randint(0, 4), randint(0, 4)];
 }
 
 
-
+//Refreshes the screen based on the current playerPosition and food
 function refreshScreen() {
     basic.clearScreen()
     led.toggle(playerPosition[0], playerPosition[1]);
@@ -18,8 +20,12 @@ function refreshScreen() {
     led.toggle(randomfood[0], randomfood[1]);
 }
 
+
+// starts game by refreshing screen, so it actually toggles the pixels
 refreshScreen();
 
+
+// up button functionality by changing the y by -1
 input.onLogoEvent(TouchButtonEvent.Pressed, function() {
     if (playerPosition[1] === 0) {
         playerPosition[1] = 4;
@@ -33,7 +39,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function() {
 })
 
 
-
+// down button by changing the y by +1
 input.onPinPressed(TouchPin.P2, function() {
     if (playerPosition[1] === 4) {
         playerPosition[1] = 0;
@@ -45,6 +51,9 @@ input.onPinPressed(TouchPin.P2, function() {
     }
 
 })
+
+
+// right button by changing the x by 1
 input.onButtonPressed(Button.B, function () {
     if (playerPosition[0] === 4) {
         playerPosition[0] = 0;
@@ -57,7 +66,7 @@ input.onButtonPressed(Button.B, function () {
 });
 
 
-
+// left button by changing the x by -1
 input.onButtonPressed(Button.A, function () {
     if (playerPosition[0] === 0) {
         playerPosition[0] = 4;
@@ -69,5 +78,7 @@ input.onButtonPressed(Button.A, function () {
     }
 });
 
+
+//this is irrelevant
 //let exampleTuple: [number, number] = [5, 6]
 //let value: number = (exampleTuple as any[])[0]
